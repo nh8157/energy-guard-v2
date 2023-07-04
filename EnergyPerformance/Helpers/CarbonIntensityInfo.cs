81792a8a1ad3031cf884db0f253f096357d060a9
Sheldon Chen <nh8157@hotmail.com>
2023-07-04 12:14:24 +0100

    :white_check_mark: Added carbon intensity class

---
+++ EnergyPerformance/Helpers/CarbonIntensityInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyPerformance.Helpers;
+public class CarbonIntensityInfo
+{
+    private double _carbonIntensity;
+    private string _location;
+    public string Location
+    {
+        get => _location;
+        set => _location = value;
+    }
+    public double CarbonIntensity
+    {
+        get => _carbonIntensity;
+        set => _carbonIntensity = value;
+    }
+    public CarbonIntensityInfo()
+    {
+        CarbonIntensity = 100;
+    }
+}
