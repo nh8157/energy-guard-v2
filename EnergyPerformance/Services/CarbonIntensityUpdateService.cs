45f70c9376e2295a1d306828320a9dd4c7cfaf87
zcaband <zcaband@ucl.ac.uk>
2023-07-10 20:47:44 +0100

    add automatical fetch of country and postcode

--- EnergyPerformance/Services/CarbonIntensityUpdateService.cs
+++ EnergyPerformance/Services/CarbonIntensityUpdateService.cs
@@ -7,6 +7,14 @@ using EnergyPerformance.Helpers;
 using Microsoft.Extensions.Hosting;
 using EnergyPerformance.Models;
 using EnergyPerformance.Core.Contracts.Services;
+using Windows.Devices.Geolocation;
+using Windows.Services.Maps;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using IPinfo;
+using IPinfo.Models;
+using System.Net;
+
 
 namespace EnergyPerformance.Services;
 class CarbonIntensityUpdateService : BackgroundService
@@ -18,6 +26,9 @@ class CarbonIntensityUpdateService : BackgroundService
     private readonly IFileService _fileService;
     private const string _defaultApplicationDataFolder = "EnergyPerformance/ApplicationData";
     private readonly string _localApplicationData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+    private readonly string IPInfoToken;
+    private string ip;
+    private readonly IPinfoClient client;
 
     public double CarbonIntensity
     {
@@ -25,15 +36,30 @@ class CarbonIntensityUpdateService : BackgroundService
         set => _carbonIntensityInfo.CarbonIntensity = value;
     }
 
-    public string Country => _locationInfo.Country;
-    public string PostCode => _locationInfo.PostCode;
+    public string Country
+    {
+        get => _locationInfo.Country;
+        set => _locationInfo.Country = value;
+    }
 
+    public string PostCode
+    {
+        get => _locationInfo.PostCode;
+        set => _locationInfo.PostCode = value;
+    }
 
-    public CarbonIntensityUpdateService(CarbonIntensityInfo carbonIntensityInfo, LocationInfo locationInfo,IFileService fileService)
+    public LocationInfo LocationInfo => _locationInfo;
+
+    public CarbonIntensityUpdateService(CarbonIntensityInfo carbonIntensityInfo, IFileService fileService)
     {
         _carbonIntensityInfo = carbonIntensityInfo;
         _locationInfo = new LocationInfo();
         _fileService = fileService;
+        IPInfoToken = "45f7eb1ccaffe7";
+        client = new IPinfoClient.Builder()
+        .AccessToken(IPInfoToken)
+        .Build();
+        ip = string.Empty;
     }
 
     protected async override Task ExecuteAsync(CancellationToken stoppingToken)
@@ -48,22 +74,26 @@ class CarbonIntensityUpdateService : BackgroundService
     private async Task DoAsync()
     {
         Debug.WriteLine("Retrieving live carbon intensity");
-        if(_locationInfo.Country!="Unknown"&& _locationInfo.PostCode!="Unknown")
+        IPinfoClient client = new IPinfoClient.Builder()
+        .AccessToken(IPInfoToken)
+        .Build();
+        if(ip == null)
         {
-            if (Country == "Great Britain")
-            {
-                await FetchLiveCarbonIntensity();
-            }
-            else
-            {
-                Debug.WriteLine("Other countries and regions are currently not supported");
-            }
+            ip = await GetIPAddress();
         }
-        // Only supports retrieving carbon intensity within the UK
-        else
+
+        if (_locationInfo.Country=="Unknown"|| _locationInfo.PostCode=="Unknown")
         {
             App.GetService<DebugModel>().AddMessage("Getting Location");
             await GetLocation();
+        }
+        if (Country == "United Kingdom")
+        {
+            await FetchLiveCarbonIntensity();
+        }
+        else
+        {
+            Debug.WriteLine("Other countries and regions are currently not supported");
         }
         Debug.WriteLine($"Current carbon intensity: {_carbonIntensityInfo.CarbonIntensity}");
         App.GetService<DebugModel>().AddMessage($"Current carbon intensity: {_carbonIntensityInfo.CarbonIntensity}");
@@ -106,30 +136,28 @@ class CarbonIntensityUpdateService : BackgroundService
     }
     public async Task GetLocation()
     {
-        string _applicationDataFolder = Path.Combine(_localApplicationData, _defaultApplicationDataFolder);
-        if (File.Exists(Path.Combine(_applicationDataFolder, "Location.json")))
-        {
-            App.GetService<DebugModel>().AddMessage("Read Location From Local Storage");
-            _locationInfo = await Task.Run(() => _fileService.Read<LocationInfo>(_applicationDataFolder, "Location.json"));
-        }
-        else
-        {
-            //add country and postcode
-            await UpdateLocation("Great Britain", "WC1E");
-        }
+        IPResponse ipResponse = await client.IPApi.GetDetailsAsync(ip);
+        string country = ipResponse.CountryName;
+        string postal = ipResponse.Postal;
         App.GetService<DebugModel>().AddMessage("Location Updated");
+        Country = country;
+        PostCode = postal;
     }
-    private async Task UpdateLocation(string country, string postcode)
+
+    static async Task<string> GetIPAddress()
     {
-        string _applicationDataFolder = Path.Combine(_localApplicationData, _defaultApplicationDataFolder);
-        if (File.Exists(Path.Combine(_applicationDataFolder, "Location.json")))
+        string apiUrl = "https://ipinfo.io/ip";
+
+        using (HttpClient client = new HttpClient())
         {
-            await Task.Run(() => _fileService.Delete(_applicationDataFolder, "Location.json"));
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                string ipAddress = await response.Content.ReadAsStringAsync();
+
+                return ipAddress.Trim();
+            }
         }
-        App.GetService<DebugModel>().AddMessage("here");
-        _locationInfo = new LocationInfo(country, postcode);
-        App.GetService<DebugModel>().AddMessage("Store Location To Local");
-        await Task.Run(() => _fileService.Save<LocationInfo>(_applicationDataFolder, "Location.json", _locationInfo));
+        return string.Empty;
     }
-    
 }
