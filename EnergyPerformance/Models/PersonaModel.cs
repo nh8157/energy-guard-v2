e987cad2a2e57b23a78cb606072a4153a2bad2ce
Sheldon Chen <nh8157@hotmail.com>
2023-07-18 17:48:54 +0100

    :pop: Added some placeholder code for enabling/disabling CPU/GPU settings. Further improvements is needed

--- EnergyPerformance/Models/PersonaModel.cs
+++ EnergyPerformance/Models/PersonaModel.cs
@@ -41,6 +41,12 @@ public class PersonaModel
         _nextPersonaId += 1;
     }
 
+    public List<(string, float)> ReadPersonaAndRating()
+    {
+        List<(string, float)> profiles = _allPersonas.Select(persona => (persona.Path, ConvertSettingsToRating(persona.CpuSetting, persona.GpuSetting))).ToList();
+        return profiles;
+    }
+
     public void UpdatePersona(int personaId, string path, float energyRating)
     {
         _allPersonas.ForEach(persona =>
@@ -54,9 +60,54 @@ public class PersonaModel
         });
     }
 
-    public bool RemovePersona(int personaId)
+    public bool DeletePersona(int personaId)
     {
         return _allPersonas.RemoveAll(persona => persona.Id == personaId) > 0 ? true : false;
+    }
+
+    public bool EnablePersona(int personaId)
+    {
+        var index = _allPersonas.FindIndex(persona => persona.Id == personaId);
+        if (!IsEnabled && index != -1)
+        {
+            var persona = _allPersonas[index];
+            // we need to research how to verify if a process is currently running
+            // if it is not running, then the following lines should not be executed
+            EnableCpuSetting(persona.Path, persona.CpuSetting);
+            EnableGpuSetting(persona.GpuSetting);
+            return true;
+        }
+        return false;
+    }
+
+    public bool DisablePersona(int personaId)
+    {
+        var index = _allPersonas.FindIndex(persona => persona.Id == personaId);
+        if (IsEnabled && index != -1)
+        {
+            var persona = _allPersonas[index];
+            DisableCpuSetting(persona.Path, persona.CpuSetting);
+            DisableGpuSetting(persona.GpuSetting);
+            return true;
+        }
+        return false;
+    }
+
+    // we might want to move these methods into their own classes
+    private void EnableCpuSetting(string path, int cpuSetting)
+    {
+    }
+
+    private void DisableCpuSetting(string path, int cpuSetting)
+    {
+    }
+
+    private void EnableGpuSetting(int gpuSetting)
+    {
+    }
+
+    private void DisableGpuSetting(int gpuSetting)
+    {
     }
 
     private int ConvertRatingToCpuSetting(float energyRating)
