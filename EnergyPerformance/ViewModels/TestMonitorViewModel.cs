f17fcb2f7730972d9251916b2fd8b2f8499e891d
Xibo Wang <18601271489@163.com>
2023-07-23 00:53:37 +0800

    inset history values

--- EnergyPerformance/ViewModels/TestMonitorViewModel.cs
+++ EnergyPerformance/ViewModels/TestMonitorViewModel.cs
@@ -1,40 +1,64 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
 using LiveChartsCore;
+using EnergyPerformance.Helpers;
+using EnergyPerformance.Models;
+using LiveChartsCore.Kernel;
+using LiveChartsCore.Kernel.Sketches;
+using LiveChartsCore.Measure;
 using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
 using LiveChartsCore.SkiaSharpView.Painting;
 using SkiaSharp;
+using EnergyPerformance.Models;
 
 namespace EnergyPerformance.ViewModels;
 public partial class TestMonitorViewModel : ObservableObject
 {
     private readonly Random _random = new();
+    private readonly EnergyUsageModel _model;
 
     public TestMonitorViewModel()
     {
-        var trend = 100;
-        var values = new List<int>();
-
-        for (var i = 0; i < 30; i++)
+       
+        var values = new List<float>();
+        //_model = App.GetService<EnergyUsageModel>();
+        var logs = _model.GetDailyEnergyUsageLogs();
+        for (int i=0;i<=30;++i)
         {
-            trend += _random.Next(-30, 50);
-            values.Add(trend);
+            if (i < logs.Count) values.Add(logs[i].PowerUsed);
+            else values.Add(0);
+            Debug.WriteLine(values[i]);
+          
         }
+        var historySeries = new ColumnSeries<float>
+        {
+            Values = values
+        };
+        historySeries.ChartPointPointerDown += OnPointerDown;
 
         Series = new ISeries[]
         {
-            new ColumnSeries<int>
-            {
-                Values = values
-            }
+            historySeries
         };
 
         XAxes = new[] { new Axis() };
+        Debug.WriteLine("123");
     }
 
     public ISeries[] Series
     {
         get;
+    }
+
+
+    private void OnPointerDown(IChartView chart, ChartPoint<float, RoundedRectangleGeometry, LabelGeometry>? point)
+    {
+        if (point?.Visual is null) return;
+        //point.Visual.Fill = new SolidColorPaint(SKColors.Red);
+        chart.Invalidate(); // <- ensures the canvas is redrawn after we set the fill
+        Debug.Write($"CLick on {point.SecondaryValue}");
     }
 
     public Axis[] XAxes
