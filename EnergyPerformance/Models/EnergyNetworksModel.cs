0fca5eca54142a15b2b359075f98de2091dcf195
Computer Science Student <uczrsld@ucl.ac.uk>
2023-08-01 21:58:26 +0100

    Merge class objects related to EnergyRateService

--- EnergyPerformance/Models/EnergyNetworksModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace EnergyPerformance.Models;
-
-
-public record class EnergyNetworksModel(
-    [property: JsonPropertyName("Postcode")] string Postcode,
-    [property: JsonPropertyName("DNOCode")] string DnoCode);
