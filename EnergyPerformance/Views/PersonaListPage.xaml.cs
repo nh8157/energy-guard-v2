a47cc739bf76047bbb8003bc38498ca88f3f4ed8
Jules Rodrigues <jules.rodrigues11@gmail.com>
2023-08-17 14:16:44 +0100

    :lipstick: :fire: Removed edit button from persona list page

--- EnergyPerformance/Views/PersonaListPage.xaml.cs
+++ EnergyPerformance/Views/PersonaListPage.xaml.cs
@@ -32,14 +32,6 @@ public sealed partial class PersonaListPage : Page
         Frame.Navigate(typeof(CustomisePersonaPage), PersonaList.SelectedIndex);    
     }
 
-    // Function that is called when the Edit Persona button is clicked
-    // Navigates to the Customise Persona Page
-    // Note - No parameter is passed, as we want to add a new persona
-    private void NavigateToEditPage (object sender, RoutedEventArgs e)
-    {
-        Frame.Navigate(typeof(CustomisePersonaPage));
-    }
-
     private void NavigateToAddPage(object sender, RoutedEventArgs e)
     {
         Frame.Navigate(typeof(AddPersonaPage));
