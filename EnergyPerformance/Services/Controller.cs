439c581c936bf8554d7a739cf2ae0467f1a9cbad
Lombardo Paredes <lombardo.calles.22@ucl.ac.uk>
2023-08-18 10:51:37 +0100

    Remove CLI Wrapper

---
+++ EnergyPerformance/Services/Controller.cs
@@ -0,0 +1,29 @@
+﻿namespace EnergyPerformance.Services;
+
+public class Controller
+{
+    public int PerformanceCoreCount()
+    {
+        return 4;
+    }
+
+    public int EfficiencyCoreCount() 
+    {
+        return 4;
+    }
+
+    public int TotalCoreCount()
+    {
+        return 12;
+    }
+
+    public void MoveAllAppsToHybridCores()
+    {
+        return;
+    }
+
+    public void ResetToDefaultCores()
+    {
+        return;
+    }
+}
